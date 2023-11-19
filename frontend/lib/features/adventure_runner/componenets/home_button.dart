e69cb66e31f074b160e79e5920a24d8ffe4b5007
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2023-11-19 06:55:29 +0530

    add home option

---
+++ frontend/lib/features/adventure_runner/componenets/home_button.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flutter/material.dart';
+import 'package:frontend/features/adventure_runner/adventure_runner.dart';
+
+class HomeButton extends SpriteComponent
+    with HasGameRef<AdventureRunner>, TapCallbacks {
+  HomeButton();
+
+  final margin = 32;
+  final buttonSize = 64;
+
+  @override
+  FutureOr<void> onLoad() {
+    sprite = Sprite(game.images.fromCache('HUD/Home.png'));
+    position = Vector2(game.size.x - 64, 20);
+    priority = 10;
+    scale = Vector2.all(2);
+    return super.onLoad();
+  }
+
+  void closeGame() {
+    Navigator.pop(gameRef.buildContext!);
+  }
+
+  @override
+  void onTapDown(TapDownEvent event) {
+    closeGame();
+    super.onTapDown(event);
+  }
+
+  @override
+  void onTapUp(TapUpEvent event) {
+    closeGame();
+    super.onTapUp(event);
+  }
+}
